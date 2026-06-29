cc3c8ae22e4509a3d8a963873fcd4e522dbc56f0
Aksheen Rathod <aksheenrathod@Aksheens-MacBook-Air.local>
2026-06-28 19:33:58 -0700

    Fixing chroma ports

--- backend/rag.py
+++ backend/rag.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
 from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
@@ -16,10 +17,13 @@ if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY is not set - check your .env file")
 
 # path where chroma will store its data on disk
-CHROMA_PATH = os.path.join(os.path.dirname(__file__), "chroma_store")
+CHROMA_HOST = os.getenv("CHROMA_SERVER_HOST", "chroma.railway.internal")
+CHROMA_PORT = int(os.getenv("CHROMA_SERVER_PORT", 8000))
 
+# This connects to the separate Chroma service instead of a local folder
+chroma_client = chromadb.HttpClient(host=CHROMA_HOST, port=CHROMA_PORT)
 
-
+collection = chroma_client.get_or_create_collection(name="your_collection_name")
 def load_and_chunk(file_path):
     loader = PyPDFLoader(file_path=file_path) #docloader
     pages = loader.load()
@@ -32,9 +36,9 @@ def load_and_chunk(file_path):
 def embed_and_store(chunks, document_id):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     vector_store = Chroma(
+        client=chroma_client,
         collection_name=f"doc_{document_id}",
-        embedding_function=embeddings,
-        persist_directory=CHROMA_PATH  # Where to save data locally, remove if not necessary
+        embedding_function=embeddings
     )
     vector_store.add_documents(chunks)
     return vector_store
@@ -42,9 +46,9 @@ def embed_and_store(chunks, document_id):
 def get_vector_store(document_id):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     vector_store = Chroma(
+        client=chroma_client,
         collection_name=f"doc_{document_id}",
         embedding_function=embeddings,
-        persist_directory=CHROMA_PATH  # Where to save data locally, remove if not necessary
     )
     return vector_store
 
