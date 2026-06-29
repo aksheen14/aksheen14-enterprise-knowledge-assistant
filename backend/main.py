5d0348ba581558affc87a9789915750a19c15ed7
Aksheen Rathod <aksheenrathod@Aksheens-MacBook-Air.local>
2026-06-28 19:16:00 -0700

    Deploying to Railway and fixing bugs

--- backend/main.py
+++ backend/main.py
@@ -268,6 +268,6 @@ def delete_document(document_id):
         return jsonify({"error": f"failed to delete document: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     
     app.run(host="0.0.0.0", port=port)
